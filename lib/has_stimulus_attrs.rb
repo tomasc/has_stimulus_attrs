31816aadc5611b51e4fcd0da55f5b03dd786d605
Tomáš Celizna <mail@tomascelizna.com>
2026-02-13 17:47:05 +0100

    accept Proc & Symbol for controller:

--- lib/has_stimulus_attrs.rb
+++ lib/has_stimulus_attrs.rb
@@ -27,16 +27,21 @@ module HasStimulusAttrs
 
     def has_stimulus_controller(name = controller_name, **options)
       key = :controller
-      val = name
+      val = case name
+            when Proc then instance_exec(&name)
+            when Symbol then send(name)
+            else name
+      end
 
       prepend___has_stimulus___method(key, val, **options)
     end
 
     def has_stimulus_action(event, action, controller: nil, **options)
       controller = case controller
-                  when Proc then instance_exec(&controller)
-                  else controller
-                  end
+                   when Proc then instance_exec(&controller)
+                   when Symbol then send(value)
+                   else controller
+      end
 
       key = :action
       val = -> {
@@ -52,8 +57,9 @@ module HasStimulusAttrs
 
     def has_stimulus_class(name, value, controller: nil, **options)
       controller = case controller
-                  when Proc then instance_exec(&controller)
-                  else controller
+                   when Proc then instance_exec(&controller)
+                   when Symbol then send(value)
+                   else controller
       end
 
       key = -> { stimulus_class((controller || controller_name), name, "N/A").keys.first }
@@ -70,8 +76,9 @@ module HasStimulusAttrs
 
     def has_stimulus_outlet(name, value, controller: nil, **options)
       controller = case controller
-                  when Proc then instance_exec(&controller)
-                  else controller
+                   when Proc then instance_exec(&controller)
+                   when Symbol then send(value)
+                   else controller
       end
 
       key = -> { stimulus_outlet((controller || controller_name), name, "N/A").keys.first }
@@ -89,8 +96,9 @@ module HasStimulusAttrs
 
     def has_stimulus_param(name, value, controller: nil, **options)
       controller = case controller
-                  when Proc then instance_exec(&controller)
-                  else controller
+                   when Proc then instance_exec(&controller)
+                   when Symbol then send(value)
+                   else controller
       end
 
       key = -> { stimulus_param((controller || controller_name), name, "N/A").keys.first }
@@ -108,8 +116,9 @@ module HasStimulusAttrs
 
     def has_stimulus_target(name, controller: nil, **options)
       controller = case controller
-                  when Proc then instance_exec(&controller)
-                  else controller
+                   when Proc then instance_exec(&controller)
+                   when Symbol then send(value)
+                   else controller
       end
 
       key = -> { stimulus_target((controller || controller_name), name).keys.first }
@@ -120,8 +129,9 @@ module HasStimulusAttrs
 
     def has_stimulus_value(name, value = nil, controller: nil, **options)
       controller = case controller
-                  when Proc then instance_exec(&controller)
-                  else controller
+                   when Proc then instance_exec(&controller)
+                   when Symbol then send(value)
+                   else controller
       end
 
       key = -> { stimulus_value((controller || controller_name), name, "N/A").keys.first }
@@ -148,20 +158,20 @@ module HasStimulusAttrs
               if options.key?(:if)
                 cond = options[:if]
                 cond_value = case cond
-                           when Proc then instance_exec(&cond)
-                           when Symbol, String then send(cond)
-                           else cond
-                           end
+                             when Proc then instance_exec(&cond)
+                             when Symbol, String then send(cond)
+                             else cond
+                end
                 return super() unless cond_value
               end
 
               if options.key?(:unless)
                 cond = options[:unless]
                 cond_value = case cond
-                           when Proc then instance_exec(&cond)
-                           when Symbol, String then send(cond)
-                           else cond
-                           end
+                             when Proc then instance_exec(&cond)
+                             when Symbol, String then send(cond)
+                             else cond
+                end
                 return super() if cond_value
               end
 
@@ -182,30 +192,30 @@ module HasStimulusAttrs
             end
           end
         )
-        
+
         # Then, ensure memoization is always at the top
         ensure_memoization_at_top
       end
-      
+
       def ensure_memoization_at_top
         # Remove any existing memoization module
         if const_defined?(:StimulusMemoization, false)
           remove_const(:StimulusMemoization)
         end
-        
+
         # Create a new memoization module at the top
         memoization_module = Module.new do
           def dom_data
             return @_stimulus_dom_data if defined?(@_stimulus_dom_data)
             @_stimulus_dom_data = super
           end
-          
+
           def reset_dom_data_cache!
             remove_instance_variable(:@_stimulus_dom_data) if defined?(@_stimulus_dom_data)
             super if defined?(super)
           end
         end
-        
+
         const_set(:StimulusMemoization, memoization_module)
         prepend(memoization_module)
       end
